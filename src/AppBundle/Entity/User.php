65190f8cef4b988ef42d9c3102bbf0b94a7b1c12
odilon <orafanambinantsoa@bocasay.com>
2018-02-02 16:17:55 +0100

    MAJBDD /login

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="utilisateur")
+ * @UniqueEntity("email")
+ * @UniqueEntity("username")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+}
