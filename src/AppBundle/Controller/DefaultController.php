67382c99115ba210f1f1fffb480f511ecd0333d3
odilon <orafanambinantsoa@bocasay.com>
2018-02-08 12:54:00 +0100

    getToken in url /oauth/v2/token2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Manager\ClientManager;
+use OAuth2\OAuth2ServerException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,7 +20,7 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return new Response(
             $this->renderView('home/index.html.twig', [
-                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
             ]),
             200,
             []
@@ -25,3 +28,4 @@ class DefaultController extends Controller
 
     }
 }
+
---
+++ src/AppBundle/Manager/BaseManager.php
@@ -0,0 +1,115 @@
+<?php
+namespace AppBundle\Manager;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+/**
+ * Class BaseManager
+ * @package AppBundle\Manager
+ */
+abstract class BaseManager
+{
+    /**
+     * @var EntityManagerInterface $entityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    protected $repository;
+
+    /**
+     *
+     * @var type
+     */
+    protected $class;
+
+    /**
+     *
+     * @param EntityManagerInterface $entityManager
+     * @param Router $router
+     *
+     */
+    public function __construct(EntityManagerInterface $entityManager, $class)
+    {
+        $this->entityManager = $entityManager;
+        $this->class = $class;
+        $this->repository = $this->entityManager->getRepository($this->class);
+    }
+
+    /**
+     * @param $entity
+     *
+     * @return mixed
+     */
+    public function save($entity)
+    {
+        $this->entityManager->persist($entity);
+
+        return $entity;
+    }
+
+    /**
+     * @param $entity
+     *
+     * @return mixed
+     */
+    public function saveAndFlush($entity)
+    {
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+
+        return $entity;
+    }
+
+    /**
+     * @param $entity
+     *
+     * @return mixed
+     */
+    public function delete($entity)
+    {
+        $this->entityManager->remove($entity);
+        $this->flushAndClear();
+
+        return true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function flushAndClear()
+    {
+        $this->entityManager->flush();
+    }
+
+    public function createNew()
+    {
+        $class = $this->class;
+
+        return new $class();
+    }
+
+    public function findAll()
+    {
+        return $this->repository->findAll();
+    }
+
+    public function find($id)
+    {
+        return  $this->repository->findOneBy(['id' => $id]);
+    }
+
+    public function findBy(array $_criteria, array $_orderBy = null, $_limit = null, $_offset = null)
+    {
+        return $this->repository->findBy($_criteria, $_orderBy, $_limit, $_offset);
+    }
+
+    public function findOneBy(array $criteria)
+    {
+        return  $this->repository->findOneBy($criteria);
+    }
+
+}
