966f69e694f5f1cb5a1d2bf7fd3a4e8e10efdfa2
Nasolo RANDIANINA <nrandianina@bocasay.com>
2018-04-12 14:19:47 +0300

    download file

--- src/AppBundle/Entity/FileDownload.php
+++ src/AppBundle/Entity/FileDownload.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Entity for the table my_fichier_download
@@ -55,6 +56,7 @@ class FileDownload
      * @var \DateTime
      *
      * @ORM\Column(name="created_at", type="datetime")
+     * @Gedmo\Timestampable(on="create")
      */
     private $createdAt;
 
